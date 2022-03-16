0a3efaa456f628365ae47c230cee22cf103e117c
Hemant Panchal <hemu1474@gmail.com>
2022-03-16 23:09:51 +0530

    Update Chi-Square test.R

--- Chi-Square test.R
+++ Chi-Square test.R
@@ -1,10 +1,3 @@
-# Chi-Square test
-# It used to determine whether any association of two variables is there or not
-
-#will import csv.file which include treatment vs. improvement
-#as per table we have to check whether treatment and improvement has any relationship or not.
-#Like person who is under medical care or treatment is improving after treatment and even after giving treatment they are not improving. 
-
 read.csv(file.choose())
 
 #assign as chi_data
